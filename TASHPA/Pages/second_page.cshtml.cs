47466a587e65e06184f669e746a6a073b42ed76c
BeerAmy <beeramiphy@gmail.com>
2024-09-23 10:04:28 +0300

    second and thide pages

---
+++ TASHPA/Pages/second_page.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TASHPA.Pages
+{
+    public class second_pageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ TASHPA/Pages/third_page.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TASHPA.Pages
+{
+    public class third_pageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
